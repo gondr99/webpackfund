0024e146646cb5070c157cef0d0141775d6fab6d
gondr99 <gondr99@gmail.com>
2019-06-02 18:29:40 +0900

    그래프까지 완료

--- App/Fund.js
+++ App/Fund.js
@@ -1,16 +1,23 @@
 import $ from 'jquery';
-
+const {PI, min} = Math;
 export default class Fund {
     constructor(el, data){
         this.el = el; //엘레멘트 넣어주고
         let template = this.makeTemplate(data);
-        el.append(template);
+        el.innerHTML = template;
+        
+        this.canvas = el.querySelector("canvas");
+        this.title = el.querySelector(".fund-title");
+        this.desc = el.querySelector(".description");
 
-        this.canvas = el.find("canvas");
-        this.title = el.find(".fund-title");
-        this.desc = el.find(".description");
+        //캔버스의 크기를 스타일에서의 크기와 동일하게 맞춰준다.
+        this.ctx = this.canvas.getContext("2d");        
+        this.resize();
 
-        console.log(this.canvas, this.title, this.desc);
+        this.current = 0; //애니메이션 드로우를 위한 값
+        this.value = data.percent;
+        this.lineWidth = 25; //선두께
+
     }
 
     makeTemplate(data) {
@@ -23,5 +30,57 @@ export default class Fund {
                     <p class="description">${data.description}</p>
                 </div>
             </div>`;
+    }
+
+    animateDraw() {
+        this.current = 0;
+        let intId = setInterval(()=>{
+            this.current++;
+            if(this.current >= this.value) {
+                this.current = this.value;
+                clearInterval(intId);
+            }
+            this.draw(this.current);
+        }, 1000 / 30);
+    }
+
+    draw(p){
+        let ctx = this.ctx;
+        ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+        let x = this.canvas.width / 2;
+        let y = this.canvas.height / 2; //중점좌표 잡고
+        let r = min(x, y) - 10; //반지름 잡고
+
+        ctx.fillStyle = "#ddd";
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.arc(x, y, r, 0, 2 * PI);
+        ctx.fill();
+
+        ctx.fillStyle = "rgb(101, 171, 243, 1)";
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+
+        //100 : 2 * PI = p : x
+        // 2* PI * p / 100 = x
+        ctx.arc(x, y, r, - PI / 2, -PI / 2 + 2 * PI * p / 100);
+        ctx.fill();
+        
+        ctx.fillStyle = "#fff";
+        ctx.beginPath();
+        ctx.arc(x, y, r - this.lineWidth, 0, 2 * PI);
+        ctx.fill();
+        
+        ctx.fillStyle = "#000";
+        ctx.font = "25px Arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        ctx.fillText(p + "%", x, y);
+    }
+
+    resize() {
+        this.canvas.width = this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
+        this.draw(this.value);
     }
 }
--- App/MainApp.js
+++ App/MainApp.js
@@ -2,32 +2,67 @@ import $ from 'jquery';
 import Fund from './Fund';
 
 export default class MainApp {
-    constructor(){
+    constructor() {
         this.sectionList = $("section");
-        this.eventHandle();
-        this.resize();
+        this.currentSection = 0;
+        this.scrolling = false;
 
         this.fundDataList = [
-            {title:"시험펀드1", description:"펀드에 대한 설명 1"},
-            {title:"시험펀드2", description:"펀드에 대한 설명 2"},
-            {title:"시험펀드3", description:"펀드에 대한 설명 3"},
-            {title:"시험펀드4", description:"펀드에 대한 설명 4"}
+            { title: "시험펀드1", description: "펀드에 대한 설명 1", percent: 43 },
+            { title: "시험펀드2", description: "펀드에 대한 설명 2", percent: 75 },
+            { title: "시험펀드3", description: "펀드에 대한 설명 3", percent: 50 },
+            { title: "시험펀드4", description: "펀드에 대한 설명 4", percent: 84 },
+            { title: "시험펀드5", description: "펀드에 대한 설명 5", percent: 20 }
         ];
 
         this.topFund = $(".top-fund-list");
         this.topFundList = [];
         this.fundDataList.forEach(x => {
             let fundBox = $(`<div class="fund-box"></div>`).appendTo(this.topFund);
-            
-            this.topFundList.push(new Fund(fundBox, x));
+            //제이쿼리 객체에서 분리해서 일반 DOM만 보낸다.
+            this.topFundList.push(new Fund(fundBox[0], x));
         });
+
+        this.eventHandle();
+        this.resize();
     }
 
-    eventHandle(){
-        $(window).on("resize", e=> this.resize() );
+    eventHandle() {
+        $(window).on("resize", e => this.resize());
+        window.addEventListener("wheel", e => this.scroll(e));
+        //$(window).on("wheel", e => this.scroll(e));
     }
 
     resize() {
         this.sectionList.height(window.innerHeight);
+        this.topFundList.forEach(x => x.resize());
+    }
+
+    scroll(e) {
+        if(this.scrolling) return;
+        
+        let next = null;
+        if (e.deltaY > 0) {
+            //스크롤이 내려가는 중
+            if (this.currentSection >= this.sectionList.length - 1) return; //마지막이라면 무시
+            this.currentSection++;
+            next = this.sectionList.eq(this.currentSection).offset().top;
+        } else {
+            //스크롤이 올라가는중
+            if (this.currentSection <= 0) return; //처음이라면 무시
+            this.currentSection--;
+            next = this.sectionList.eq(this.currentSection).offset().top;
+        }
+        if (next != null) {
+            this.scrolling = true;
+            $("html, body").stop().animate({ "scrollTop": next }, 1500, "swing", ()=>{
+                this.scrolling = false;    
+            });
+            this.beforeTop = next;
+
+            if(this.currentSection == 1){ //펀드 리스트를 보는 곳에 도달했다면  
+                this.topFundList.forEach(x => x.animateDraw() );
+            }
+        }
     }
 }
